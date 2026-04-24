ac06591edbda131934e2d2b415e2c52142589186
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-04-24 23:59:07 +0530

    chore: select dom elements

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const contactForm = document.getElementById("contactForm");
+const username = contactForm.elements["username"];
+const email = contactForm.elements["email"];
+const message = contactForm.elements["message"];
+const nameError = document.getElementById("nameError");
+console.log(nameError)
+const emailError = document.getElementById("emailError");
+const messageError = document.getElementById("messageError");
+
+const formData = new FormData(contactForm);
+console.log(formData)
+// console.log(contactForm.elements['username']);
+
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    if (!username.value) {
+        console.log(nameError.classList);
+    }
+})
