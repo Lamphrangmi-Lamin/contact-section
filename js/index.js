5c58ab50c8850f3fd03ebd94a70171eb94518c9b
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-04-28 07:05:57 +0530

    feat: implement first-error focus handling

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const username = contactForm.elements["username"];
 const email = contactForm.elements["email"];
 const message = contactForm.elements["message"];
 const usernameError = document.getElementById("usernameError");
-// console.log(usernameError)
 const emailError = document.getElementById("emailError");
 const messageError = document.getElementById("messageError");
 
@@ -13,21 +12,38 @@ console.log(formData)
 
 contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    let isValid = true;
+    let firstErrorField = null;
     
+    // Username validation
     if (!username.value) {
+        if (!firstErrorField) firstErrorField = username;
+        isValid = false;
         showError(username);
     }
+
+    // Email address validation
     if (!email.value) {
+        if (!firstErrorField) firstErrorField = email;
+        isValid = false;
         showError(email);
     }
+
+    // Message validation
     if (!message.value) {
+        if (!firstErrorField) firstErrorField = message;
+        isValid = false;
         showError(message);
     }
+    if (!isValid) {
+        firstErrorField.focus();
+        return;
+    };
 })
 
 function showError(input) {
     const field = input.parentElement;
     const error = field.querySelector(".error");
     error.classList.remove("hidden");
-    // input.focus();
 }
