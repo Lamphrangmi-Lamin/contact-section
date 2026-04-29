d69f11db8c62df1c786205e04e24e164fd99a249
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-04-29 07:19:39 +0530

    feat: add dynamic error styles to input on focus

--- js/index.js
+++ js/index.js
@@ -7,57 +7,107 @@ const emailError = document.getElementById("emailError");
 const messageError = document.getElementById("messageError");
 
 const formData = new FormData(contactForm);
-console.log(formData)
+console.log(formData);
 // console.log(contactForm.elements['username']);
 
 contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let isValid = true;
-    let firstErrorField = null;
-    
-    // Username validation
-    if (!username.value) {
-        if (!firstErrorField) firstErrorField = username;
-        isValid = false;
-        showError(username, "Name is required.");
-    }
+  let isValid = true;
+  let firstErrorField = null;
 
-    // Email address validation
-    if (!email.value) {
-        if (!firstErrorField) firstErrorField = email;
-        isValid = false;
-        showError(email, "Email is required.");
-    }
+  // Username validation
+  if (username.value.trim() === "") {
+    if (!firstErrorField) firstErrorField = username;
+    isValid = false;
+    addErrorStyle(isValid, username);
+    showError(username, "Name is required.");
+  }
 
-    if (email.value && !isEmailValid(email.value)) {
-        if (!firstErrorField) firstErrorField = email;
-        isValid = false;
-        showError(email, "Please enter a valid email address.");
-    }
+  // Email address validation
+  if (!email.value) {
+    if (!firstErrorField) firstErrorField = email;
+    isValid = false;
+    addErrorStyle(isValid, username);
+    showError(email, "Email is required.");
+  }
 
-    // Message validation
-    if (!message.value) {
-        if (!firstErrorField) firstErrorField = message;
-        isValid = false;
-        showError(message, "Message is required");
-    }
-    if (!isValid) {
-        firstErrorField.focus();
-        return;
-    };
-})
+  if (email.value && !isEmailValid(email.value)) {
+    if (!firstErrorField) firstErrorField = email;
+    isValid = false;
+    addErrorStyle(isValid, email);
+    showError(email, "Please enter a valid email address.");
+  }
 
+  // Message validation
+  if (!message.value) {
+    if (!firstErrorField) firstErrorField = message;
+    isValid = false;
+    addErrorStyle(isValid, message);
+    showError(message, "Message is required");
+  }
+  if (!isValid) {
+    firstErrorField.focus();
+    return;
+  }
+});
+
+
+// EventListeners
+username.addEventListener("input", (e) => {
+  if (e.target.value) {
+    removeErrorStyle(username);
+    showError(username, "");
+    return;
+  }
+});
+
+email.addEventListener("input", (e) => {
+  if (isEmailValid(e.target.value)) {
+    removeErrorStyle(email);
+    showError(email, "");
+    return;
+  }
+});
+
+message.addEventListener("input", (e) => {
+  if (e.target.value) {
+    removeErrorStyle(message);
+    showError(message, "");
+    return;
+  }
+});
+
+// Helper functions
 function showError(input, message) {
-    const field = input.parentElement;
-    const error = field.querySelector(".error");
-    error.innerText = message;
-    error.classList.remove("hidden");
+  const field = input.parentElement;
+  const error = field.querySelector(".error");
+  error.innerText = message;
+  error.classList.remove("hidden");
 }
 
 function isEmailValid(email) {
-    const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-    return regex.test(email);
+  const regex =
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  return regex.test(email);
 }
 
-// console.log(isEmailValid('test@gcom.c'))
+function addErrorStyle(isValid, inputField) {
+  if (!isValid) {
+    inputField.classList.remove("focus:border-indigo-600");
+    inputField.classList.add("focus:border-red-600");
+
+    inputField.classList.remove("focus:outline-indigo-200");
+    inputField.classList.add("focus:outline-red-50");
+    return;
+  }
+}
+
+function removeErrorStyle(inputField) {
+  inputField.classList.remove("focus:border-red-600");
+  inputField.classList.add("focus:border-indigo-600");
+
+  inputField.classList.remove("focus:outline-red-50");
+  inputField.classList.add("focus:outline-indigo-200");
+  return;
+}
